f77ad7a5cba7a96316bf07fcde8f54fc5d763bff
DragonFighter603 <75088766+DragonFIghter603@users.noreply.github.com>
2023-02-04 21:50:42 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -9,7 +9,7 @@ cargo rustc -Z build-std=core --release -- --emit obj=target/scoletite_os.o --em
 rem cargo rustc -Z build-std=core -- --emit obj=target/scoletite_os_raw.o --emit asm=target/scoletite_os_raw.asm
 rem objcopy target/scoletite_os_raw.o target/scoletite_os.o --prefix-alloc-sections='.rust'
 
-ld.lld asm/boot.o target/scoletite_os.o -T link.ld --oformat binary -o img.bin
+ld.lld asm/boot.o target/scoletite_os.o -T link.ld --oformat binary -e _boot -o img.bin
 
 rem qemu-system-x86_64 -drive format=raw,file=img.bin
 qemu-system-x86_64 -kernel img.bin
